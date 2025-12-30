01ecb0e79b8c16716df9be2e38e585271d51ee9e
ambujkumar <ambuj.kumar@juspay.in>
2025-12-30 17:07:01 +0530

    added graceful shutdown?

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"students/internal/config"
+	"syscall"
+	"time"
+)
 
 func main(){
-	fmt.Println("welcome to students api")
+	// load config
+	cfg:= config.MustLoad()
+	// database setup
+	// router setup
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func (w http.ResponseWriter, r *http.Request)  {
+		w.Write([]byte("welcome to students api"))
+	})
+	// server setup
+
+	server := http.Server {
+		Addr: cfg.HTTPServer.Addr,
+		Handler: router,
+	}
+	slog.Info("server started", slog.String("addres", cfg.HTTPServer.Addr))
+
+	done:= make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGTERM)
+
+	go func(){
+		err:= server.ListenAndServe()
+		if err != nil{
+			log.Fatalf("failed to start server, %s", err.Error())
+		}
+	}()
+
+	<- done
+
+	slog.Info("shutting down server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err:= server.Shutdown(ctx); err != nil{
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
+	}	
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,28 +1,29 @@
 package config
 
 import (
+	"flag"
 	"log"
 	"os"
 
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
-type HTTPServer struct{
-	Addr string
+type HTTPServer struct {
+	Addr string `yaml:"addr" env-required:"true"`
 }
 
-type Config struct{
-	Env string `yaml: "env" env:"ENV" env-required:"true" env-default:"production"`
-	StoragePath string `yaml:"storage_path" env_required:"true"`
-	HTTPServer `yaml:"http_server`
+type Config struct {
+	Env         string     `yaml:"env" env:"ENV" env-default:"production"`
+	StoragePath string     `yaml:"storage_path" env-required:"true"`
+	HTTPServer  HTTPServer `yaml:"http_server"`
 }
 
 func MustLoad() *Config{
 	var configPath string
 	configPath = os.Getenv("CONFIG_PATH")
 	if configPath == ""{
-		flags:= flags.String("config", "", "path to the configuration file")
-		flags.Parse()
+		flags := flag.String("config", "", "path to the configuration file")
+		flag.Parse()
 
 		configPath = *flags
 
